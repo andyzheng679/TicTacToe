c85d9084f0a962d9a0b405438acd5b186f44d711
andyzheng679 <andyzheng679@gmail.com>
2024-03-12 13:32:36 -0400

    rows

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,15 +4,49 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    private Character[][] matrix;
+
     public Board(Character[][] matrix) {
+        this.matrix = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        int xCounter = 0;
+        boolean xRowWinner = false;
+
+
+
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                if(matrix[i][j] == 'X'){
+                    xCounter++;
+                }
+            }
+            if(xCounter == 3){
+                xRowWinner = true;
+            }
+        }
+
+        return xRowWinner;
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        int oCounter = 0;
+        boolean oRowWinner = false;
+
+
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                if(matrix[i][j] == 'O'){
+                    oCounter++;
+                }
+            }
+            if(oCounter == 3){
+                oRowWinner = true;
+            }
+        }
+
+        return oRowWinner;
     }
 
     public Boolean isTie() {
@@ -20,7 +54,15 @@ public class Board {
     }
 
     public String getWinner() {
-        return null;
+        String winner = "";
+
+
+        if(isInFavorOfO() && !isInFavorOfX()){
+            winner = "O";
+        } else if (!isInFavorOfO() && isInFavorOfX()) {
+            winner = "X";
+        }
+        return winner;
     }
 
 }
